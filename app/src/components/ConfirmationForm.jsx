05a0cb5b8a7a682e07a2eb6ed34e876002ddd6e4
royeradames <royeraadames@gmail.com>
2020-08-27 22:49:25 -0400

    added inputs to the confirm form

--- app/src/components/ConfirmationForm.jsx
+++ app/src/components/ConfirmationForm.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
+import { Form } from 'semantic-ui-react'
 
+const options = [
+    { key: 'e', text: 'Entree', value: 'entree' },
+    { key: 'dt', text: 'Dessert', value: 'dessert' },
+    { key: 'ds', text: 'Drinks', value: 'drinks' },
+]
 export default function ConfirmationForm() {
     return (
-        <div className='container confirmation-form-container'>
-            <h1>Confirmation Page</h1>
-            <form onSubmit="handleSubmit">
-                <h2>Select the food you will bring</h2>
-                <input type="checkbox" name="foodList" id="foodList" />
-                <label htmlFor="foodList">{'foodlist name'}</label>
-                <button type="submit" className='btn submit-btn'>Submit</button>
-            </form>
-        </div>
+        // <div className='container confirmation-form-container'>
+        //     <h1>Confirmation Page</h1>
+        <Form>
+            <Form.Input fluid label='Your Name' placeholder='Name' />
+            <Form.Input fluid label='Food Item' placeholder='Food Item' />
+            <Form.Group widths='equal'>
+                <Form.Select
+                    fluid
+                    label='Food Item Type'
+                    options={options}
+                    placeholder='Food Item Type'
+                />
+                <Form.Group >
+                    <Form.Button fluid type='button'>Add more</Form.Button>
+                    <Form.Button fluid type='button'>Remove</Form.Button>
+                </Form.Group>
+
+            </Form.Group>
+            <Form.Checkbox label='I agree to the Terms and Conditions' required />
+            <Form.Button>Submit</Form.Button>
+        </Form>
+        /* </div> */
     )
 }
--- app/src/components/Potlucks.jsx
+++ app/src/components/Potlucks.jsx
@@ -3,75 +3,14 @@ import { useHistory } from "react-router-dom"
 import { axiosWithAuth } from '../utils/axiosWithAuth'
 
 export default function Potlucks() {
+    //helper variables
+    const { push } = useHistory()
+
     //states
     const [potluckList, setpotluckList] = useState()
 
     //helper functions
-    const { push } = useHistory()
 
-    // //states
-    // const potluckList = useSelector(state => state)
-
-    // const renderPotlucks = () => {
-    //     //check if there is potluck on the list
-    //     const isListNotHere = !(potluckList[0].potluckName)
-
-    //     debugger
-    //     return isListNotHere ? <p>Start hosting a new Potluck</p> : potluckList.map(aPotluck => {
-    //         //make potluck name url friendly
-    //         //lowercase
-    //         //nospace
-    //         let urlPotluckName = aPotluck.potluckName.replace(/\s+/g, '')
-    //         urlPotluckName = urlPotluckName.toLowerCase()
-
-    //         return (
-    //             <section className="upcoming-potluck">
-    //                 <h2>
-    //                     Potluck Name: {aPotluck.potluckName}
-    //                 </h2>
-    //                 <p>
-    //                     Date: {aPotluck.date}
-    //                 </p>
-    //                 <p>
-    //                     Food List: {aPotluck.foodList}
-    //                 </p>
-    //                 <p>
-    //                     Location: {aPotluck.location}
-    //                 </p>
-    //                 <p>
-    //                     Invited: {aPotluck.invited.map((anInvited, index) => {
-    //                     const lastIndex = (aPotluck.invited.length - 1)
-    //                     const isLastNameOnList = lastIndex === index
-
-    //                     //return the last name without a comman , so the list don't look uncompleted
-    //                     if (isLastNameOnList) return `${anInvited.name}`
-
-    //                     //return name on the invitation list
-    //                     return `${anInvited.name},`
-    //                 })}
-    //                 </p>
-    //                 <p>
-    //                     Invited Confirmed Attendence: {aPotluck.myFoodList}
-    //                 </p>
-    //                 <p>
-    //                     My food list: {aPotluck.myFoodList}
-    //                 </p>
-
-    //                 {/* Only if it bellongs to the user */}
-    //                 <button onClick={() => push(`/edityourpotlock/${aPotluck.id}`)} className="btn edit">Edit Your Potlucker</button>
-    //                 <button onClick={() => {
-    //                     dispatch({ type: DELETE, payload: { id: aPotluck.id } })
-    //                 }
-    //                 } className="btn delete">Delete Potlucker</button>
-    //                 <button className="btn public-btn" onClick={() => {
-    //                     push(`/${aPotluck.id}/${urlPotluckName}`)
-    //                 }
-    //                 }>See live potluck</button>
-    //             </section>
-    //         )
-    //     })
-
-    // }
     const renderPotlucks = () => {
         //check if there is potluck on the list
         debugger
